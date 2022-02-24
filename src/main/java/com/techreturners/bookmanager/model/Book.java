631b3a5f3a456732ff0d8fcd7220fbf0b6d94daf
revathisekar35 <revathisekar2010@gmail.com>
2022-02-24 03:45:34 +0000

    Refactor : Removed unwanted code

--- src/main/java/com/techreturners/bookmanager/model/Book.java
+++ src/main/java/com/techreturners/bookmanager/model/Book.java
@@ -17,60 +17,21 @@ import javax.persistence.Id;
 @Builder
 public class Book {
 
-    @Id
-    @GeneratedValue
-    @Column(updatable = false, nullable = false)
-    Long id;
+	@Id
+	@GeneratedValue
+	@Column(updatable = false, nullable = false)
+	Long id;
 
-    @Column
-    String title;
+	@Column
+	String title;
 
-    @Column
-    String description;
+	@Column
+	String description;
 
-    @Column
-    String author;
+	@Column
+	String author;
 
-    @Column
-    Genre genre;
+	@Column
+	Genre genre;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
-	public Genre getGenre() {
-		return genre;
-	}
-
-	public void setGenre(Genre genre) {
-		this.genre = genre;
-	}
 }
