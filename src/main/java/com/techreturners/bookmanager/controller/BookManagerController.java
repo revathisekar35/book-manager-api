81b5113905bfd3aca75a25e07f6fbb488e6d667f
revathisekar35 <revathisekar2010@gmail.com>
2022-02-27 04:22:33 +0000

    Refactor : Exception handling refactor for controller method

--- src/main/java/com/techreturners/bookmanager/controller/BookManagerController.java
+++ src/main/java/com/techreturners/bookmanager/controller/BookManagerController.java
@@ -1,18 +1,14 @@
 package com.techreturners.bookmanager.controller;
 
-import com.techreturners.bookmanager.exception.ApiRequestException;
 import com.techreturners.bookmanager.model.Book;
 import com.techreturners.bookmanager.service.BookManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/v1/book")
@@ -29,45 +25,29 @@ public class BookManagerController {
 
 	@GetMapping({ "/{bookId}" })
 	public ResponseEntity<Book> getBookById(@PathVariable Long bookId) {
-		try {
-			return new ResponseEntity<>(bookManagerService.getBookById(bookId), HttpStatus.OK);
-		} catch (NoSuchElementException e) {
-			throw new ApiRequestException("Oops Request book is not available");
-		}
+		return new ResponseEntity<>(bookManagerService.getBookById(bookId), HttpStatus.OK);
 	}
 
 	@PostMapping
 	public ResponseEntity<Book> addBook(@RequestBody Book book) {
-		try {
-			Book newBook = bookManagerService.insertBook(book);
-			HttpHeaders httpHeaders = new HttpHeaders();
-			System.out.println("newBook---" + newBook);
-			httpHeaders.add("book", "/api/v1/book/" + newBook.getId().toString());
-			return new ResponseEntity<>(newBook, httpHeaders, HttpStatus.CREATED);
-		} catch (Exception e) {
-			throw new ApiRequestException("Input Json is not valid");
-		}
+		Book newBook = bookManagerService.insertBook(book);
+		HttpHeaders httpHeaders = new HttpHeaders();
+		System.out.println("newBook---" + newBook);
+		httpHeaders.add("book", "/api/v1/book/" + newBook.getId().toString());
+		return new ResponseEntity<>(newBook, httpHeaders, HttpStatus.CREATED);
 	}
 
 	// User Story 4 - Update Book By Id Solution
 	@PutMapping({ "/{bookId}" })
 	public ResponseEntity<Book> updateBookById(@PathVariable("bookId") Long bookId, @RequestBody Book book) {
-		try {
-			bookManagerService.updateBookById(bookId, book);
-			return new ResponseEntity<>(bookManagerService.getBookById(bookId), HttpStatus.OK);
-		} catch (NoSuchElementException e) {
-			throw new ApiRequestException("Oops Request book is not available");
-		}
+		bookManagerService.updateBookById(bookId, book);
+		return new ResponseEntity<>(bookManagerService.getBookById(bookId), HttpStatus.OK);
 	}
 
 	@DeleteMapping({ "/{bookId}" })
 	public ResponseEntity<Book> deleteBookById(@PathVariable Long bookId) {
-		try {
-			bookManagerService.deleteBookById(bookId);
-			return new ResponseEntity<>(HttpStatus.OK);
-		} catch (EmptyResultDataAccessException e) {
-			throw new ApiRequestException("Oops Request book is not available");
-		}
+		bookManagerService.deleteBookById(bookId);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 }
--- src/main/java/com/techreturners/bookmanager/exception/ApiExceptionHandler.java
+++ src/main/java/com/techreturners/bookmanager/exception/ApiExceptionHandler.java
@@ -3,10 +3,13 @@ package com.techreturners.bookmanager.exception;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class ApiExceptionHandler {
@@ -14,6 +17,18 @@ public class ApiExceptionHandler {
 
 	@ExceptionHandler(value = { ApiRequestException.class })
 	public ResponseEntity<Object> handleApiRequestException(ApiRequestException e) {
+		ApiException apiException = new ApiException(e.getMessage(), badRequest, ZonedDateTime.now(ZoneId.of("Z")));
+		return new ResponseEntity<>(apiException, badRequest);
+	}
+	
+	@ExceptionHandler(value = { NoSuchElementException.class })
+	public ResponseEntity<Object> NoSuchElementException(NoSuchElementException e) {
+		ApiException apiException = new ApiException(e.getMessage(), badRequest, ZonedDateTime.now(ZoneId.of("Z")));
+		return new ResponseEntity<>(apiException, badRequest);
+	}
+	
+	@ExceptionHandler(value = { EmptyResultDataAccessException.class })
+	public ResponseEntity<Object> EmptyResultDataAccessException(EmptyResultDataAccessException e) {
 		ApiException apiException = new ApiException(e.getMessage(), badRequest, ZonedDateTime.now(ZoneId.of("Z")));
 		return new ResponseEntity<>(apiException, badRequest);
 	}
